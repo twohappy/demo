f4d6335e5f2c79f7050c741219f9173399ee54fc
林金晨 Lynn <qq744392269@gmail.com>
2021-03-26 14:08:13 +0800

    1. 完成底部背景和导航栏

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Note from './assets/sidebar/note.png';
+import Folder from './assets/sidebar/folder.png';
+import Upcoming from './assets/sidebar/upcoming.png';
+import Avatar from './assets/sidebar/avatar.png';
+import Timeline from './assets/sidebar/timeline.png';
+import Plus from './assets/content/plusicon.png';
+import Clock from './assets/content/clock.png';
+import Bell from './assets/content/bell.png';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div className="column">
+				<div className="column-sidebar">
+					 <ul className='nav-list'>
+							<li><img src={Upcoming} /></li>
+							<li><img src={Folder} /></li>
+							<li><img src={Timeline} /></li>
+							<li><img src={Note} /></li>
+						</ul>
+					<p className="nav-avatar"><img src={Avatar} /></p>
+					</div>
+				<div className="column-content">
+					<p className="plus"><img src={Plus} /></p>
+
+				</div>
+			</div>
+		);
+	}
+}
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<App />,
+	document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
