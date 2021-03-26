e860435c4ac730c1dfad9fedd2d4c4178fb5e54b
林金晨 Lynn <qq744392269@gmail.com>
2021-03-26 17:49:46 +0800

    1. 初步完成，时间对齐那边有点问题。

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ import Timeline from './assets/sidebar/timeline.png';
 import Plus from './assets/content/plusicon.png';
 import Clock from './assets/content/clock.png';
 import Bell from './assets/content/bell.png';
+import Search from './assets/content/search.png';
+import Tick from './assets/content/tick.png';
+import Point from './assets/content/point.png';
 
 class App extends React.Component {
 	render() {
@@ -24,13 +27,84 @@ class App extends React.Component {
 					<p className="nav-avatar"><img src={Avatar} /></p>
 					</div>
 				<div className="column-content">
-					<p className="plus"><img src={Plus} /></p>
-
+          <div>
+            <button className="plus"><img src={Plus} /></button>
+            <button className="search"><img src={Search} /></button>
+            <button className="bell"><img src={Bell} /></button>
+          </div>
+          <div className="note">
+            <p className="backgroundtext">TODAY</p>
+            <p className="note-text">TODAY</p>
+            <p className="note-number">6 Tasks</p>
+            <table  className="note-list">
+              <tr>
+                <td className="note-list-icon"><button className="nonebutton"><img src={Tick} /></button></td>
+                <td className="note-list-border">
+                  <button className="note-list-border-red">Hight</button>
+                </td>
+                <td className="note-list-text">Cook Eggs Don T Boil</td>
+                <td className="note-list-clock">
+                  {/*这个地方 试了很多方法，都没做到图片和文字居中对齐，僵住了*/}
+                  <button className="nonebutton">
+                  {/*这里时钟和字体颜色，逻辑不太明确，是少于1小时为红色？*/}
+                    <img src={Clock} />
+                    <button className="nonebutton">Due in 30 min</button>
+                  </button>
+                </td>
+                <td className="note-list-icon">
+                  <button className="nonebutton">
+                  <img src={Point} />
+                  </button>
+                </td>
+              </tr>
+            </table>
+            <table  className="note-list">
+              <tr>
+                <td className="note-list-icon"><button className="nonebutton"><img src={Tick} /></button></td>
+                <td className="note-list-text">Thrill Friends And Family With A Make It Yourself Pizza Party</td>
+                <td className="note-list-clock">
+                  <button className="nonebutton">
+                    <img src={Clock} />
+                    <button className="nonebutton">Due in 2 hours</button>
+                  </button>
+                </td>
+                <td className="note-list-icon">
+                  <button className="nonebutton">
+                  <img src={Point} />
+                  </button>
+                </td>
+              </tr>
+            </table>     
+            <table  className="note-list">
+              <tr>
+                <td className="note-list-icon"><button className="nonebutton"><img src={Tick} /></button></td>
+                <td className="note-list-border">
+                  <button className="note-list-border-grey">Medium</button>
+                </td>
+                <td className="note-list-border">
+                  <button className="note-list-border-blue">Cook</button>
+                </td>                
+                <td className="note-list-text">Smarter Food Choices 101 Tips For Busy Women</td>
+                <td className="note-list-clock">
+                  <button className="nonebutton">
+                    <img src={Clock} />
+                    <button className="nonebutton">Due in 9 hours</button>
+                  </button>
+                </td>
+                <td className="note-list-icon">
+                  <button className="nonebutton">
+                  <img src={Point} />
+                  </button>
+                </td>
+              </tr>
+            </table>        
+          </div>
 				</div>
 			</div>
 		);
 	}
 }
+
 
 
 
